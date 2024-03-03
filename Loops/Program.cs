1f0289352847e42cc0606a607ae068455b6a93c9
Hedwig29 <156532418+Hedwig29@users.noreply.github.com>
2024-03-03 10:32:32 -0600

    Abstract homework

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -2,9 +2,44 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            FruitStore freshFruits = new FruitStore();
+            string fruit = "Melon";
+            freshFruits.OrderProduct(fruit, 5, "FreshCo ");
+            Console.WriteLine($"Продукт годен: {freshFruits.IsProductExpried(fruit)}");
+            Console.WriteLine($"Цена продукта: {freshFruits.GetProductPrice(fruit)}");
+            Console.WriteLine($"Страна производитель: {freshFruits.GetProductInfo(fruit)}");
+
+            LiquorMart liquorMarket = new LiquorMart();
+            string alco = "Brandy";
+            Console.WriteLine();
+            liquorMarket.OrderProduct(alco, 20, "Island Rum");
+            Console.WriteLine($"Продукт годен: {liquorMarket.IsProductExpried(alco)}");
+            Console.WriteLine($"Цена продукта: {liquorMarket.GetProductPrice(alco)}");
+            Console.WriteLine($"Страна производитель: {liquorMarket.GetProductInfo(alco)}");
+
+            CheeseStore cheeseStore = new CheeseStore();
+            string cheese = "Cheese Gauda";
+            Console.WriteLine();
+            cheeseStore.OrderProduct(cheese, 20, "French Cheese");
+            Console.WriteLine($"Продукт годен: {cheeseStore.IsProductExpried(cheese)}");
+            Console.WriteLine($"Цена продукта: {cheeseStore.GetProductPrice(cheese)}");
+            Console.WriteLine($"Страна производитель: {cheeseStore.GetProductInfo(cheese)}");
+
+
+
+
+
+
+
+
         }
+   
+
+       
+
+
+
     }
 }
---
+++ Loops/StoreManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    public abstract class StoreManager
+    {
+        public abstract bool IsProductExpried(string product);
+        public abstract int GetProductPrice(string product);
+        public abstract string GetProductInfo(string productName);
+        public void OrderProduct(string productName, int qnty, string supplierName)
+        {
+            Console.WriteLine($"Продукт {productName} заказан у {supplierName}, в количестве {qnty} единиц");
+        }
+    }
+
+    public class FruitStore : StoreManager
+    {
+        public override bool IsProductExpried(string product)
+        {
+            Random rnd = new Random();
+            int numb = rnd.Next(1, 101);
+            if (numb > 50) return true;
+            else return false;
+        }
+
+        public override int GetProductPrice(string product)
+        {
+            Random rnd = new Random();
+            int numb = rnd.Next(1, 11) * 10;
+            return numb;
+        }
+
+        public override string GetProductInfo(string productName)
+        {
+            string madeIn = "India";
+            return madeIn;
+        }
+    }
+
+    public class LiquorMart : StoreManager
+    {
+        public override bool IsProductExpried(string product)
+        {
+            DateTime today = DateTime.Today;
+            today = today.AddYears(2);
+            Console.WriteLine(today.ToString("D"));
+            return true;
+        }
+
+        public override int GetProductPrice(string product)
+        {
+            Random rnd = new Random();
+            int numb = rnd.Next(5, 21) * 16;
+            return numb;
+        }
+
+        public override string GetProductInfo(string productName)
+        {
+            string madeIn = productName + " Scotland";
+            return madeIn;
+        }
+    }
+
+    public class CheeseStore : StoreManager
+    {
+        public override bool IsProductExpried(string product)
+        {
+            if (product.Contains("Gauda"))
+                return true;
+            else
+                return false;
+        }
+
+        public override int GetProductPrice(string product)
+        {
+            Random rnd = new Random();
+            int numb = rnd.Next(33, 56) / 4;
+            return numb;
+        }
+
+        public override string GetProductInfo(string productName)
+        {
+            string madeIn = "Spain";
+            if (productName.Contains("blue"))
+            {
+                madeIn = "France";
+            }
+            return madeIn;
+        }
+    }
+}

025f3c7a06d086c142c6db0fe755b67d87f84e26
Hedwig29 <156532418+Hedwig29@users.noreply.github.com>
2024-03-07 17:10:16 -0600

    Homework is done

---
+++ Loops/Extension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    public static class Extension
+    {
+        public static void WordCounter(this string text)
+        {
+            string[] arrayWords = text.Split(' ');
+
+            Console.WriteLine($"В строке {arrayWords.Length} слов");
+        }
+
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static List<string> GetListOfUniqNames(this List<string> names)
+        {
+            return names.Distinct().ToList();
+        }
+    }
+}
--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,10 +1,136 @@
-﻿namespace Loops
+﻿using System.Numerics;
+using System.Reflection;
+
+namespace Loops
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello, World!");
+            //# Исключение и обработка.
+            // Задача 1: Деление на ноль
+
+            
+            static int Divide()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Введите первое число:");
+
+                        int num1 = int.Parse(Console.ReadLine());
+
+                       
+                        Console.Write("Введите второе число:");
+
+                        
+                        int num2 = int.Parse(Console.ReadLine());
+
+                       
+                        return num1 / num2;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Невозмoжно делить на 0");
+
+                        Console.WriteLine("Попытайтесь еще раз");
+
+                        continue;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Для ввода доступны только цифры");
+
+                        Console.WriteLine("Попытайтесь еще раз");
+
+                        continue;
+                    }
+
+                }
+            }
+
+            
+
+            Console.WriteLine( Divide());
+
+            // Задача 2: Обработка исключения , открытие файла
+
+            static void FileReader()
+            {
+                try
+                {
+                    File.Open("Test.txt", FileMode.Open);
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("Файл не найден");
+                }
+
+            }
+
+            // Задача 3: Обработка ошибок при парсинге чисел
+
+            static int Converter()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Введите число:");
+
+                        return Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Попытайтесь снова");
+
+                        continue;
+                    }
+
+                }
+
+            }
+            Console.WriteLine("\n#TryCatch №3\n");
+
+            Console.WriteLine(Converter());
+
+
+            //Задача 1: Расширение для строк
+
+            string text = "“Аппетит приходит во время еды.”";
+
+            Console.WriteLine("\n#Extension №1\n");
+
+            text.WordCounter();
+
+            // Задача 2: Расширение для чисел
+
+            int number = 12345678;
+
+            bool isEven = number.IsEven();
+
+            Console.WriteLine("\n#Extension №2\n");
+
+            Console.WriteLine($"{number} четное: {isEven}");
+
+            // Задача 3: Расширение для списков
+
+            List<string> names = new List<string>()
+        {"Аглая", "Арина", "Полина", "Яна", "Владимир", "Эмма", "Стефания", "Макар", "Полина" ,"Эмма"};
+
+            List<string> uniqNames = names.GetListOfUniqNames();
+
+            Console.WriteLine("\n#Extension №3\n");
+
+            foreach (var name in uniqNames)
+            {
+                Console.WriteLine(name);
+            }
+
         }
+        
+            
     }
 }
+
